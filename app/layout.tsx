1581a4ba1a0dfb014869210fd224417431d3e8f5
Mohammed Fazil Momin <mominmasty1214@gmail.com>
2025-07-18 11:51:43 +0530

    checking

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata, Viewport } from "next"
 import { Open_Sans } from "next/font/google"
 import { ErrorBoundary } from "@/components/error-boundary"
 import { ThemeProvider } from "@/components/theme-provider"
-
 import { CacheCleanup } from "@/components/cache-cleanup"
 
 import AdminRoute from "@/components/admin-route"
@@ -91,7 +90,6 @@ export default function RootLayout({
       </head>
       <body className={`${openSans.className} antialiased min-h-screen bg-light-blue dark:bg-[#161616]`}>
         <ErrorBoundary>
-
           <ThemeProvider>
             <ModalProvider>
               <ClientLayout>
@@ -99,7 +97,6 @@ export default function RootLayout({
               </ClientLayout>
             </ModalProvider>
           </ThemeProvider>
-          </NotificationProvider>
         </ErrorBoundary>
         <CacheCleanup />
       </body>
